91ec10479b0d6d928837ad4c44c194d3339ed7ea
jonkan <joel.engstrom17@gmail.com>
2024-12-17 21:21:58 +0100

    update

--- python/process_images.py
+++ python/process_images.py
@@ -78,7 +78,7 @@ def process_country_images(country_code):
 if __name__ == "__main__":
 
     # List of countries to process
-    countries = ['se', 'no', 'de']
+    countries = ['se', 'no', 'de', 'fi']
     
     for country in countries:
         process_country_images(country)
